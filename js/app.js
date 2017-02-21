805273be175f26cfcce3fa2083c605c4bc8f736c
codemattcode <codemattcode@gmail.com>
2017-02-21 14:56:54 +0000

    add partials

--- js/app.js
+++ js/app.js
@@ -3,13 +3,13 @@
 // JQUERY
 
 $(document).ready(function(){
-    $( window ).on( "load", function() {
-      $(".brand-logo__img").removeClass("img.brand-logo__img--remove-load-padding")
-    }),
 
     $(window).scroll(function() {
         var o = $(window).scrollTop();
   o >= 150 ? $(".brand-logo__img").addClass("brand-logo__img--hide") : $(".brand-logo__img").removeClass("brand-logo__img--hide")
+
+  o <= 100 ? $(".brand-logo__img").addClass("scroll") : $(".brand-logo__img").removeClass("scroll")
+
   o >= 150 ? $(".brand-name__title").addClass("brand-name__title--scroll-up") : $(".brand-name__title").removeClass(".brand-name__title--scroll-down")
     })
 });
