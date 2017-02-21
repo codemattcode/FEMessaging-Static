1884da038044e03d1cbc742344e5c4c8d25ef9cb
codemattcode <codemattcode@gmail.com>
2017-02-21 13:57:07 +0000

    add scroll hide show logo functionality

--- js/app.js
+++ js/app.js
@@ -1,21 +1,13 @@
-function showSize() {
-    var w = window.innerWidth;
-    document.getElementById('size').innerHTML = "W: " + w;
-}
-showSize();
-
-// (function(){
-
-//   window.console.log('hello world!');
-
-// }());
-
+"use strict";
 
 // JQUERY
 
 $(document).ready(function(){
 
-  // code
-
+    $(window).scroll(function() {
+        var o = $(window).scrollTop();
+  o >= 150 ? $(".brand-logo__img").addClass("brand-logo__img--hide") : $(".brand-logo__img").removeClass("brand-logo__img--hide")
+  o >= 150 ? $(".brand-name__title").addClass("brand-name__title--scroll-up") : $(".brand-name__title").removeClass(".brand-name__title--scroll-down")
+    })
 });
 
